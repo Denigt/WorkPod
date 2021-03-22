a47b01da9dc1de231d79173aa6ed7f3b88eb626a
Denigt <guerrero.raulparra@gmail.com>
2021-03-22 20:18:18 +0100

    22-03 Propuesta tablas | Primera Pantalla Sign in

--- app/src/main/java/com/example/workpod/InitActivity.java
+++ app/src/main/java/com/example/workpod/InitActivity.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class InitActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        getSupportActionBar().hide();
+        setContentView(R.layout.activity_init);
     }
 }
