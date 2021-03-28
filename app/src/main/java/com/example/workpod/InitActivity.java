3fb356b823b274213cef41bdccd0beee2a75708b
Denigt <guerrero.raulparra@gmail.com>
2021-03-28 20:07:04 +0200

    28-03 Interfaz de Signin finalizada

--- app/src/main/java/com/example/workpod/InitActivity.java
+++ app/src/main/java/com/example/workpod/InitActivity.java
@@ -2,19 +2,72 @@ package com.example.workpod;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class InitActivity extends AppCompatActivity {
-BottomNavigationView bNV;
+public class InitActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // CONTROLES DEL XML
+    private Button btnAcceder;
+    private Button btnConectar;
+    private Button btnRegistrar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_init);
-       // Toast.makeText(getApplicationContext(), Build.VERSION.RELEASE,Toast.LENGTH_LONG).show();
-       // bNV=(BottomNavigationView)
+
+        // BUSCAR LOS CONTROLES DEL XML
+        btnAcceder = findViewById(R.id.btnAcceder);
+        btnConectar = findViewById(R.id.btnConectar);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
+
+        // ESTABLECER EVENTOS PARA LOS CONTROLES
+        btnAcceder.setOnClickListener(this);
+        btnConectar.setOnClickListener(this);
+        btnRegistrar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        btnAccederOnClick(v);
+        btnConectarOnClick(v);
+        btnRegistrarOnClick(v);
+    }
+
+    // CLICK DE LOS BOTONES
+    private void btnAccederOnClick(View v){
+        if(v.getId() == btnAcceder.getId()){
+            // PROVISIONAL PARA NO CAMBIAR EL MANIFIEST
+            Intent activity = new Intent(getApplicationContext(), WorkpodActivity.class);
+            startActivity(activity);
+        }
+    }
+
+    private void btnConectarOnClick(View v){
+        if(v.getId() == btnConectar.getId()){
+
+        }
+    }
+
+    /**
+     * Inicia una actividad para registrarse al pulsar "Registrarse"
+     * @param v Vista clicada
+     */
+    private void btnRegistrarOnClick(View v){
+        if(v.getId() == btnRegistrar.getId()){
+            Intent activity = new Intent(getApplicationContext(), SigninActivity.class);
+            // Indicar la pantalla de registro
+            activity.putExtra("screen", 0);
+            startActivity(activity);
+        }
     }
 }
--- app/src/main/java/com/example/workpod/SigninActivity.java
+++ app/src/main/java/com/example/workpod/SigninActivity.java
@@ -1,14 +1,92 @@
 package com.example.workpod;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SigninActivity extends AppCompatActivity {
+public class SigninActivity extends AppCompatActivity implements View.OnClickListener {
+
+    // VARIABLES GLOBALES DE LA ACTIVIDAD
+    int pantalla;
+
+    // CONTROLES DEL XML
+    private EditText txtNombre;
+    private EditText txtApellidos;
+    private EditText txtEmail;
+    private EditText txtContrasena;
+    private EditText txtRContrasena;
+    private ImageButton btnSiguiente;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_signin);
+        overridePendingTransition(R.anim.fragment_open_enter, R.anim.fragment_close_enter);
+
+        pantalla = getIntent().getIntExtra("screen", 0);
+
+        if (pantalla == 0)
+            setContentView(R.layout.activity_signin);
+        else
+            setContentView(R.layout.activity_signin2);
+
+        // BUSCAR LOS CONTROLES DEL XML
+        if(pantalla == 0) {
+            txtNombre = findViewById(R.id.txtNombre);
+            txtApellidos = findViewById(R.id.txtApellido);
+        }else {
+            txtEmail = findViewById(R.id.txtEmail);
+            txtContrasena = findViewById(R.id.txtContrasena);
+            txtRContrasena = findViewById(R.id.txtRContrasena);
+        }
+        btnSiguiente = findViewById(R.id.btnSiguiente);
+
+        // ESTABLECER EVENTOS PARA LOS CONTROLES
+        /*if(pantalla != 0) {
+            txtContrasena.setOnFocusChangeListener(this);
+            txtRContrasena.setOnFocusChangeListener(this);
+        }*/
+        btnSiguiente.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        if(pantalla == 1) {
+            Intent activity = new Intent(getApplicationContext(), SigninActivity.class);
+            // Indicar la pantalla de registro
+            activity.putExtra("screen", 0);
+            startActivity(activity);
+            finish();
+        }
+        super.onPause();
+    }
+
+    // LISTENERS
+    @Override
+    public void onClick(View v) {
+        btnSiguienteOnClick(v);
+    }
+
+    // CLICK DE LOS BOTONES
+    /**
+     * Si es la primera pantalla para registrarse abre la segunda
+     * Si es la segunda pantalla, combrueba que los datos sean correctos, crea el usuario y abre
+     * la aplicacion
+     * @param v Vista clicada
+     */
+    private void btnSiguienteOnClick(View v){
+        if(v.getId() == btnSiguiente.getId()){
+            if(pantalla == 0) {
+                Intent activity = new Intent(getApplicationContext(), SigninActivity.class);
+                // Indicar la pantalla de registro
+                activity.putExtra("screen", 1);
+                startActivity(activity);
+                finish();
+            }
+        }
     }
 }
