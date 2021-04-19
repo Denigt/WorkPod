8342dd5caa0661a7b1217eb8adec0c815c7e29b8
Juanvi123 <juanvitj@gmail.com>
2021-04-19 17:17:02 +0200

    19-04 completando funcionalidad del fragment del historico de transacciones

--- app/src/main/java/com/example/workpod/fragments/Fragment_Transaction_History.java
+++ app/src/main/java/com/example/workpod/fragments/Fragment_Transaction_History.java
@@ -1,15 +1,14 @@
 package com.example.workpod.fragments;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.workpod.R;
 import com.example.workpod.adapters.Adaptador_LsV_Transaction_History;
@@ -17,53 +16,29 @@ import com.example.workpod.otherclass.LsV_Transaction_History;
 
 import java.util.ArrayList;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Fragment_Transaction_History#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Fragment_Transaction_History extends Fragment {
-    private ListView lsV_Transaction;
+public class Fragment_Transaction_History extends Fragment implements View.OnClickListener {
+
+    //ARRAYLIST CON TODAS LAS SESIONES DEL USUARIO
     private ArrayList<LsV_Transaction_History> aLstTransaction = new ArrayList<LsV_Transaction_History>();
+    //ARRAYLIST CON LAS ÚLTIMAS 5 SESIONES DEL USUARIO
+    private ArrayList<LsV_Transaction_History> aLstTransactionIniciales = new ArrayList<>();
+    //ARRAYLIST CON LA ÚLTIMA SESIÓN DEL USUARIO
+    private ArrayList<LsV_Transaction_History> aLstUltimaTransaction = new ArrayList<>();
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //XML
+    private ListView lsV_Transaction;
+    private ImageView iVTodasSesiones;
+    private ImageView iVUltimaSesion;
+    private ImageView iVLimpiarSesiones;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Fragment_Transaction_History() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Fragment_Transaction_History.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Fragment_Transaction_History newInstance(String param1, String param2) {
-        Fragment_Transaction_History fragment = new Fragment_Transaction_History();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -71,36 +46,123 @@ public class Fragment_Transaction_History extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment__transaction__history, container, false);
         lsV_Transaction = (ListView) view.findViewById(R.id.LsV_Transaction_History);
+        iVLimpiarSesiones = (ImageView) view.findViewById(R.id.IVLimpiarSesiones);
+        iVTodasSesiones = (ImageView) view.findViewById(R.id.IVTodasSesiones);
+        iVUltimaSesion = (ImageView) view.findViewById(R.id.IVUltimaSesion);
+
+        iVUltimaSesion.setOnClickListener(this);
+        iVTodasSesiones.setOnClickListener(this);
+        iVLimpiarSesiones.setOnClickListener(this);
         construyendo_LsV(view);
+        //PARA ACTIVAR EL MENU EMERGENTE
+        setHasOptionsMenu(true);
         return view;
     }
 
+    //SOBREESCRITURAS
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.IVTodasSesiones) {
+            final Adaptador_LsV_Transaction_History aTransaction = new Adaptador_LsV_Transaction_History(v.getContext(), aLstTransaction);
+            lsV_Transaction.setAdapter(aTransaction);
+            lsV_Transaction.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                    LsV_Transaction_History lsV_Transaction = (LsV_Transaction_History) aTransaction.getItem(i);
+                }
+            });
+        } else if (v.getId() == R.id.IVUltimaSesion) {
+            mostrarUltimaSesion(v);
+        } else if (v.getId() == R.id.IVLimpiarSesiones) {
+            limpiarSesiones(v);
+        }
+    }
+
+    //MÉTODOS
+
     /**
-     * Método para construir el ListView donde se encontrará el email, el tlfn y la dirección de la compañía
+     * Método para construir el ListView donde se encontrarán las sesiones del usuario con las cabinas workpod
      *
      * @param view instancia de la clase View
      */
     public void construyendo_LsV(View view) {
 
-        aLstTransaction.add(new LsV_Transaction_History(0, "C/piruletaaa,2", "01-01-2001", "5:20 Horas"));
-        aLstTransaction.add(new LsV_Transaction_History(1, "C/piruletaaa,2", "01-01-2001", "5:20 Horas"));
-        aLstTransaction.add(new LsV_Transaction_History(2, "C/piruletaaa,2", "01-01-2001", "5:20 Horas"));
-        aLstTransaction.add(new LsV_Transaction_History(3, "C/piruletaaa,2", "01-01-2001", "5:20 Horas"));
-        aLstTransaction.add(new LsV_Transaction_History(4, "C/piruletaaa,2", "01-01-2001", "5:20 Horas"));
-        aLstTransaction.add(new LsV_Transaction_History(5, "C/piruletaaa,2", "01-01-2001", "5:20 Horas"));
-        aLstTransaction.add(new LsV_Transaction_History(6, "C/piruletaaa,2", "01-01-2001", "5:20 Horas"));
-        aLstTransaction.add(new LsV_Transaction_History(7, "C/piruletaaa,2", "01-01-2001", "5:20 Horas"));
-        aLstTransaction.add(new LsV_Transaction_History(8, "C/piruletaaa,2", "01-01-2001", "5:20 Horas"));
-        aLstTransaction.add(new LsV_Transaction_History(9, "C/piruletaaa,2", "01-01-2001", "5:20 Horas"));
-        final Adaptador_LsV_Transaction_History aTransaction = new Adaptador_LsV_Transaction_History(view.getContext(), aLstTransaction);
+        aLstTransaction.add(new LsV_Transaction_History(0, "C/Pablo Neruda,2", "01-01-2001", "5:20 Horas"));
+        aLstTransaction.add(new LsV_Transaction_History(1, "C/Rafael Alberti,7", "01-01-2001", "5:20 Horas"));
+        aLstTransaction.add(new LsV_Transaction_History(2, "C/Albert Einstein,27", "01-01-2001", "5:20 Horas"));
+        aLstTransaction.add(new LsV_Transaction_History(3, "C/Arquímides,18", "01-01-2001", "5:20 Horas"));
+        aLstTransaction.add(new LsV_Transaction_History(4, "C/Chaikoski,47", "01-01-2001", "5:20 Horas"));
+        aLstTransaction.add(new LsV_Transaction_History(5, "C/Antonio Machado,98", "01-01-2001", "5:20 Horas"));
+        aLstTransaction.add(new LsV_Transaction_History(6, "Avnd Sócrates,41", "01-01-2001", "5:20 Horas"));
+        aLstTransaction.add(new LsV_Transaction_History(7, "C/Chopin,2", "01-01-2001", "5:20 Horas"));
+        aLstTransaction.add(new LsV_Transaction_History(8, "C/Mozart,15", "01-01-2001", "5:20 Horas"));
+        aLstTransaction.add(new LsV_Transaction_History(9, "C/Stan Lee,23", "01-01-2001", "5:20 Horas"));
+
+        //COPIAMOS EN ESTE ARRAYLIST LAS ÚLTIMAS 5 SESIONES REGISTRADAS
+        for (int i = aLstTransaction.size() - 1; i >= (aLstTransaction.size() - 5); i--) {
+            aLstTransactionIniciales.add(aLstTransaction.get(i));
+        }
+
+        //COPIAMOS EN ESTE ARRAYLIST LA ÚLTIMA SESIÓN DEL USUARIO
+        for (int i = aLstTransaction.size() - 1; i >= (aLstTransaction.size() - 1); i--) {
+            aLstUltimaTransaction.add(aLstTransaction.get(i));
+        }
+
+        //MOSTRAREMOS AL INICIAR EL FRAGMENT EL ARRAY QUE CONTIENE LAS ÚLTIMAS 5 SESIONES
+        mostrar5UltimasSesiones(view);
+    }
+
+    /**
+     * Método para mostrar las últimas 5 sesiones de worpod realizadas por el usuario
+     *
+     * @param v instancia de la clase View
+     */
+    public void mostrar5UltimasSesiones(View v) {
+        final Adaptador_LsV_Transaction_History aTransaction = new Adaptador_LsV_Transaction_History(v.getContext(), aLstTransactionIniciales);
         lsV_Transaction.setAdapter(aTransaction);
         lsV_Transaction.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
                 LsV_Transaction_History lsV_Transaction = (LsV_Transaction_History) aTransaction.getItem(i);
-                if (lsV_Transaction.getCodigo() == 1) {
-                    Toast.makeText(getActivity(), "Heyyyy", Toast.LENGTH_SHORT).show();
-                }
+            }
+        });
+    }
+
+    /**
+     * Método para mostrar sólamente la última sesión de workpod realizada por el usuario
+     *
+     * @param v instancia de la clase View.
+     */
+    public void mostrarUltimaSesion(View v) {
+        final Adaptador_LsV_Transaction_History aTransaction = new Adaptador_LsV_Transaction_History(v.getContext(), aLstUltimaTransaction);
+        lsV_Transaction.setAdapter(aTransaction);
+        lsV_Transaction.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                LsV_Transaction_History lsV_Transaction = (LsV_Transaction_History) aTransaction.getItem(i);
+
+            }
+        });
+    }
+
+    /**
+     * Método para limpiar todas las sesiones de workpod realizadas por el usuario.
+     *
+     * @param v instancia de la clase View
+     */
+    public void limpiarSesiones(View v) {
+        //LIMPIAMOS TODOS LOS ARRAYLIST
+        aLstTransaction.clear();
+        aLstUltimaTransaction.clear();
+        aLstTransactionIniciales.clear();
+
+        //MOSTRAMOS EL ARRAYLIST QUE CONTENDRÍA TODAS LAS SESIONES (QUE ESTARÁ VACÍO)
+        final Adaptador_LsV_Transaction_History aTransaction = new Adaptador_LsV_Transaction_History(v.getContext(), aLstTransaction);
+        lsV_Transaction.setAdapter(aTransaction);
+        lsV_Transaction.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                LsV_Transaction_History lsV_Transaction = (LsV_Transaction_History) aTransaction.getItem(i);
             }
         });
     }
