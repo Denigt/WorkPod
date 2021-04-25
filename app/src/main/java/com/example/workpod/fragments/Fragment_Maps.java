8340e87adec1bbc38595fb141d0d9ef11fcb78df
Denigt <guerrero.raulparra@gmail.com>
2021-04-25 20:39:59 +0200

    25-04 Icono de mi ubicacion | Boton de centrar en el mapa

--- app/src/main/java/com/example/workpod/fragments/Fragment_Maps.java
+++ app/src/main/java/com/example/workpod/fragments/Fragment_Maps.java
@@ -2,18 +2,25 @@ package com.example.workpod.fragments;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.DialogFragment;
 
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.workpod.R;
@@ -21,6 +28,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -28,15 +38,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import java.security.Permissions;
 import java.security.acl.Permission;
 
-public class Fragment_Maps extends DialogFragment implements OnMapReadyCallback {
+public class Fragment_Maps extends DialogFragment implements OnMapReadyCallback, View.OnClickListener {
 
     // CODIGOS PARA LA SOLICITUD DE PERMISOS
     private final int LOCATION_PERMISSION_CODE = 1003;
 
     // VARIABLES PARA LA GESTION DEL MAPA Y LA LOCALIZACION
+    private final double errorMedida = 0.01;
+    private final int defaultZoom = 12;
     private GoogleMap mMap;
     private LocationManager locationService;
     private LatLng posicion;
+
+    // VARIABLES PARA LOS CONTROLES DEL FRAGMENT
+    private ImageButton btnCentrar;
 
     //CONSTRUCTOR POR DEFECTO
     public Fragment_Maps() {
@@ -57,15 +72,24 @@ public class Fragment_Maps extends DialogFragment implements OnMapReadyCallback 
         }
     }
 
-    @Override
+    @Overrideº
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Introducir el Layout del fragment
         View view = inflater.inflate(R.layout.fragment_maps, container, false);
+
         // Obtener el fragment del mapa e informar de que esta listo para ser usado
         SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        // Obtener los controles de fragment
+        btnCentrar = view.findViewById(R.id.btnCentrar);
+
+        // Establecer listeners de los controles
+        btnCentrar.setOnClickListener(this);
+
+
         return view;
     }
 
@@ -77,12 +101,13 @@ public class Fragment_Maps extends DialogFragment implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        // Iniciar el hilo para solicitar la ubicacion
         try {
             locationService.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 10, new UbicacionListener());
         }catch (SecurityException e) {  }
 
         // Establecer zoom y posicion inicial del mapa (Posicion inicial puerta del Sol)
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(40.41704890982951, -3.703483401820587)));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(40.41704890982951, -3.703483401820587), defaultZoom));
     }
 
     @Override
@@ -96,12 +121,25 @@ public class Fragment_Maps extends DialogFragment implements OnMapReadyCallback 
         }
     }
 
+    // LISTENERS
+    @Override
+    public void onClick(View v) {
+            btnCentrarOnClick(v);
+    }
+
     // CLASE QUE FUNCIONARA COMO EL LISTENER DE LA UBICACION
     public class UbicacionListener implements LocationListener, Runnable {
         Marker markPosicion;
 
         public UbicacionListener() {
             markPosicion = null;
+            try {
+                Location aux = locationService.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                if (aux != null) {
+                    posicion = new LatLng(aux.getLatitude(), aux.getLongitude());
+                    getActivity().runOnUiThread(this);
+                }
+            }catch(SecurityException e) {}
         }
 
         @Override
@@ -143,12 +181,14 @@ public class Fragment_Maps extends DialogFragment implements OnMapReadyCallback 
         public void run() {
             if (!compruebaGPS()) {
                 if (posicion != null) {
-                    if (markPosicion == null)
+                    if (markPosicion == null) {
                         markPosicion = mMap.addMarker(new MarkerOptions().position(posicion).title("Mi posición"));
+                        markPosicion.setIcon(VectortoBitmap(getContext() , R.drawable.button_btn_centrar, 160, 160));
+                        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(posicion, defaultZoom));
+                    }
                     else
                         markPosicion.setPosition(posicion);
 
-                    mMap.moveCamera(CameraUpdateFactory.newLatLng(posicion));
                 }
             }
         }
@@ -171,5 +211,46 @@ public class Fragment_Maps extends DialogFragment implements OnMapReadyCallback 
 
             return error;
         }
+    }
+
+    // ONCLICK LISTENERS
+    /**
+     * Centra el mapa sobre la posicion del usuario
+     * @param v Control pulsado
+     */
+    private void btnCentrarOnClick(View v){
+        if (v.getId() == btnCentrar.getId() && posicion != null){
+            // COMPROBAR SI LA CAMARA ESTA MAS O MENOS EN LA POSICION QUE LA DEJA POR DEFECTO EL BOTON CENTRAR
+            if (!((mMap.getCameraPosition().target.latitude < posicion.latitude + errorMedida) &&
+                    (mMap.getCameraPosition().target.latitude > posicion.latitude - errorMedida) &&
+                    (mMap.getCameraPosition().target.longitude < posicion.longitude + errorMedida) &&
+                    (mMap.getCameraPosition().target.longitude > posicion.longitude - errorMedida) &&
+                    (mMap.getCameraPosition().zoom == defaultZoom)))
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(posicion, defaultZoom));
+            // SI LA CAMARA NO ESTA DESPLAZADA ACERCAR ZOOM
+            else
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(posicion, 19));
+        }
+    }
+
+    /**
+     * Transforma un vectorAsset en un bitmapDrawable
+     * @param context Contexto de la app
+     * @param drawable Imagen vectorial
+     * @param width Ancho de la imagen en px
+     * @param height Alto de la imagen en px
+     * @return Bitmap a partir del vectorAsset
+     */
+    private static BitmapDescriptor VectortoBitmap(Context context, int drawable, int width, int height){
+        // Obtener el Drawable
+        Drawable vectorImg = ContextCompat.getDrawable(context, drawable);
+        // Establecer tamanos
+        vectorImg.setBounds(0, 0, width, height);
+        Bitmap bm = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        // Dibujar en el bitmat
+        Canvas canvas = new Canvas(bm);
+        vectorImg.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bm);
     }
 }
