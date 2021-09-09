5f59e05ead224b25db5cd4d31d73b6f4e4b1d134
Juanvi123 <juanvitj@gmail.com>
2021-09-09 23:43:57 +0200

    09-09 3.5.0 empezando con FCM, push a todos los usuarios

---
+++ app/src/main/java/com/workpodapp/workpod/MyFirebaseMessagingService.java
@@ -0,0 +1,8 @@
+package com.workpodapp.workpod;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+/**
+ * Esta es la clase que será capaz de recibir las notificaciones y mediante la cual podremos manejarlas a nuestro antojo
+ */
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+}
